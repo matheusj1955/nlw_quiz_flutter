3a42dc8e6d91b1edfd986f72fc76afd21fa1fb9e
Matheus J <matheus.pro1995@gmail.com>
2021-04-26 18:24:18 -0300

    Aula 02

---
+++ lib/challenge/challenge_page.dart
@@ -0,0 +1,23 @@
+import 'package:DevQuiz/challenge/widget/question_indicator/question_indicator.dart';
+import 'package:DevQuiz/challenge/widget/quiz/quiz_widget.dart';
+import 'package:flutter/material.dart';
+
+class ChallengePage extends StatefulWidget {
+  @override
+  _ChallengePageState createState() => _ChallengePageState();
+}
+
+class _ChallengePageState extends State<ChallengePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(60),
+        child: SafeArea(top: true, child: QuestionIndicatorWidget()),
+      ),
+      body: QuizWidget(
+        title: "O que o Flutter faz em sua totalidade?",
+      ),
+    );
+  }
+}
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:DevQuiz/splash/splash_page.dart';
 import 'package:DevQuiz/widgets/appbar/app_bar_widget.dart';
 import 'package:DevQuiz/widgets/appbar/level_button/level_button_widget.dart';
+import 'package:DevQuiz/widgets/appbar/quiz_card/quiz_card_widget.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -15,18 +16,45 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBarWidget(),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        child: Column(
           children: [
-            LevelButtonWidget(
-              label: "Fácil",
-            ), LevelButtonWidget(
-              label: "Médio",
-            ), LevelButtonWidget(
-              label: "Difícil",
-            ), LevelButtonWidget(
-              label: "Perito",
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                SizedBox(
+                  height: 24,
+                ),
+                LevelButtonWidget(
+                  label: "Fácil",
+                ),
+                LevelButtonWidget(
+                  label: "Médio",
+                ),
+                LevelButtonWidget(
+                  label: "Difícil",
+                ),
+                LevelButtonWidget(
+                  label: "Perito",
+                ),
+              ],
             ),
+            SizedBox(
+              height: 24,
+            ),
+            Expanded(
+                child: GridView.count(
+                  crossAxisSpacing: 16,
+                  mainAxisSpacing: 16,
+                  crossAxisCount: 2,
+                    children: [
+                      QuizCardWidget(),
+                      QuizCardWidget(),
+                      QuizCardWidget(),
+                      QuizCardWidget(),
+                      QuizCardWidget(),
+                      QuizCardWidget()
+                    ],
+            ))
           ],
         ),
       ),
